3a8243db41044e77fa972bda741499665b6bdb1c
christinaschim <christinaschimm@gmail.com>
2023-07-24 16:47:45 -0700

    building

--- Scontract1/sContarc1e.js
+++ Scontract1/sContarc1e.js
@@ -1,3 +1,23 @@
 function greet() {
     alert("Hello World");
 }
+function connect() {
+    if (!window.ethereum) {
+        alert("No injected provider found. Install Metamask.");
+    } else {
+        try {
+         const accounts = window.ethereum.request({
+            method: "eth_requestAccounts",
+            params: []
+         });
+         const account = accounts[0];
+         const chainID = window.ethereum.request({
+         method: "eth_chainID",
+         params:[]
+        })
+        alert("Connected to account:", account, "and ChainId:", chainID);
+        }catch {
+            alert("Something went wrong connecting. Refresh and try again.");
+        }
+    }
+}
