3da360f324d20bb0c1ab36533f934e7f36f64a4e
christinaschim <christinaschimm@gmail.com>
2023-08-19 16:00:23 -0700

    finish JS

--- Scontract1/sContarc1e.js
+++ Scontract1/sContarc1e.js
@@ -1,7 +1,50 @@
-const CHAIN_ID = ;
+const CHAIN_ID = "0x5" ;
 const CHAIN_NAME = "Sepolia";
-function greet() {
+const CONTRACT_ADDRESS = "0x158eBF211B01e0a5986f4A363737956f47561379"
+async function greet() {
     alert("Hello World");
+    const url = "https://sepolia.infura.io/v3/96549a7507ed4b63befcd49cc4a02b70"
+    const infura_response = await fetch(url, {
+        method: "POST", 
+        body: JSON.stringify({
+            params:[{
+                   data:"0xef690cc0",
+                   to: CONTRACT_ADDRESS
+
+                },
+                "latest"
+          ],
+          jsonrpc: "2.0",
+          method: "eth_call",
+          id: 1
+        })
+    } );
+    console.log(infura_response);
+    const result = await infura_response.json();
+    console.log(result); 
+
+    let hex = result.result;
+    hex = String(hex).slice(2);
+    console.log("hex: ", hex);
+
+    const bytes = new Uint8Array(Math.ceil(String(hex).length / 2));
+
+
+    
+    for (let i = 0; i < bytes.length; i++) {
+        const start = i*2; 
+        const stop = start+2;
+        const BASE = 16;
+        const currentSubString = String(hex).substring(start, stop)
+        bytes [i] = parseInt(currentSubString, BASE); 
+    }
+    console.log("bytes: ", bytes);
+
+    const decoder = new TextDecoder ("ascii");
+    const message = decoder.decode(bytes);
+    console.log("message: ", message);
+    alert(message);
+
 }
 async function connect() {
     if (!window.ethereum) {
@@ -22,8 +65,8 @@ async function connect() {
          params:[]
         });
         console.log("chainId: ", chainId);
-        console.log(chainId !== CHAIN_ID);
-        console.log()
+        console.log("chainId !== CHAIN_ID", chainId !== CHAIN_ID);
+        console.log("CHAIN_ID: ")
         if (chainId !== CHAIN_ID) {
             alert(" Connected to wrong chain! Please connect to " + CHAIN_NAME)
         } else { alert("Connected to account:", + String(account), "and chainId:"+ String(chainId));
@@ -32,7 +75,7 @@ async function connect() {
 
 
         alert("Connected to account:", + String(account), "and chainId:"+ String(chainId));
-        }catch {
+        } catch {
             alert("Something went wrong connecting. Refresh and try again.");
         }
     }
